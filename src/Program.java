300c57dd9da88bb0194ad97edd3c8502d2a1edcb
chutch01 <chutch01@hotmail.com>
2015-01-29 17:03:07 -0500

    practice JOption Pane

--- src/Program.java
+++ src/Program.java
@@ -6,14 +6,40 @@
  * @author Cody Hutchinson
  *
  */
+import javax.swing.JOptionPane;
+
+
 public class Program {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstNumber = "";
+		String secondNumber = "";
+		
+		//obtain user input
 
+		try{	
+			firstNumber = JOptionPane.showInputDialog("Enter first Integer");
+			System.out.println(firstNumber);
+			secondNumber = JOptionPane.showInputDialog("Enter second Integer");
+			System.out.println(secondNumber);
+			
+			int number1 = Integer.parseInt(firstNumber);
+			int number2 = Integer.parseInt(secondNumber);
+			
+			int sum = number1 + number2;
+			
+			//JOptionPane.showMessageDialog(null, "The sum is" + sum + "The sum of two integers",);
+			JOptionPane.showMessageDialog(null, "The Sum is " + sum, "The Sum of two integers", JOptionPane.PLAIN_MESSAGE);
+		}
+		 catch(Exception e){
+			 JOptionPane.showMessageDialog(null, "Incorrect values entered" ,"Entry Error", JOptionPane.ERROR_MESSAGE);
+			 System.out.println("incorrect values entered");
+			 System.out.println(e);
+		 }
+
+	
 	}
-
 }
